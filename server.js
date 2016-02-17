fab152bceee1849e61f4e30ea6cd17848dd3aabe
Dave Baskin <dbaskin@wintellect.com>
2016-02-16 19:30:07 -0500

    Adding Babel for server code.

--- server.js
+++ server.js
@@ -1,25 +1,7 @@
-"use strict";
 
-var _ = require("lodash");
-var express = require("express");
-var app = express();
-var portNum = process.env.PORT || 9988;
+import app from "./server/routes";
 
-var vendorFiles = {
-    "es6-shim": "node_modules/es6-shim",
-    "systemjs": "node_modules/systemjs/dist",
-    "angular2": "node_modules/angular2",
-    "rxjs": "node_modules/rxjs",
-    "foundation": "node_modules/foundation-sites/dist",
-    "jquery": "node_modules/foundation-sites/node_modules/jquery/dist",
-    "what-input": "node_modules/foundation-sites/node_modules/what-input"
-};
-
-_.forEach(vendorFiles, function(path, name) {
-    app.use("/vendor/" + name, express.static(path));
-});
-
-app.use("/", express.static("src"));
+const portNum = process.env.PORT || 9988;
 
 app.listen(portNum, function() {
     console.log("Web application listening on port " + portNum);
---
+++ server/routes.js
@@ -0,0 +1,22 @@
+
+import express from "express";
+
+var app = express();
+
+var vendorFiles = {
+    "es6-shim": "node_modules/es6-shim",
+    "systemjs": "node_modules/systemjs/dist",
+    "angular2": "node_modules/angular2",
+    "rxjs": "node_modules/rxjs",
+    "foundation": "node_modules/foundation-sites/dist",
+    "jquery": "node_modules/foundation-sites/node_modules/jquery/dist",
+    "what-input": "node_modules/foundation-sites/node_modules/what-input"
+};
+
+for (let [name, path] of Object.entries(vendorFiles)) {
+    app.use("/vendor/" + name, express.static(path));
+}
+
+app.use("/", express.static("src"));
+
+export default app;
