d92f84430428179c0e6ed80985a916f9f1fe8511
Dave Baskin <dbaskin@wintellect.com>
2016-02-20 10:59:46 -0500

    Making AppStore a singleton.

--- src/app/services/app-store.ts
+++ src/app/services/app-store.ts
@@ -1,38 +1,9 @@
 
-import {Injectable} from 'angular2/core'
+import {Injectable, provide} from 'angular2/core'
 import {createStore, applyMiddleware} from 'redux/redux'
 import Rx from 'rxjs/Rx'
 import * as thunkMiddleware from 'redux-thunk/redux-thunk'
 
-function counter(state = 0, action) {
-    'use strict';
-    switch (action.type) {
-        case 'INCREMENT':
-            return state + 1
-        case 'DECREMENT':
-            return state - 1
-        default:
-            return state
-    }
-}
-
-function counterActionCreator() {
-    'use strict';
-    return dispatch => {
-        let list = [true, true, false, true, true];
-        Rx.Observable
-            .interval(500)
-            .take(list.length)
-            .map(idx => list[idx])
-            .subscribe(incr => {
-                dispatch({
-                    type: incr ? 'INCREMENT' : 'DECREMENT'
-                })
-            })
-    }
-}
-
-@Injectable()
 export class AppStore {
 
     private appStore
@@ -63,5 +34,37 @@ export class AppStore {
 
     public get currentState() {
         return this.appStore.getState();
+    }
+}
+
+export const appStoreInstance = new AppStore()
+
+provide(AppStore, appStoreInstance)
+
+function counter(state = 0, action) {
+    'use strict';
+    switch (action.type) {
+        case 'INCREMENT':
+            return state + 1
+        case 'DECREMENT':
+            return state - 1
+        default:
+            return state
+    }
+}
+
+function counterActionCreator() {
+    'use strict';
+    return dispatch => {
+        let list = [true, true, false, true, true];
+        Rx.Observable
+            .interval(500)
+            .take(list.length)
+            .map(idx => list[idx])
+            .subscribe(incr => {
+                dispatch({
+                    type: incr ? 'INCREMENT' : 'DECREMENT'
+                })
+            })
     }
 }
