f18acb6ee1aa63f672089122d19be9796f19140b
Dave Baskin <dbaskin@wintellect.com>
2016-03-13 18:59:13 -0400

    Separating layout into individual files.

--- src/app/components/demo-app/layouts/detail-only-layout.ts
+++ src/app/components/demo-app/layouts/detail-only-layout.ts
@@ -6,42 +6,8 @@ import {ImageDetailList} from '../../image-detail-list/image-detail-list'
 @Component({
     selector: 'detail-only-layout',
     directives: [TitleBar, ImageDetailList],
-    template: `
-        <div class="root-layout">
-            <div>
-                <title-bar [isLoading]="isLoading"></title-bar>
-            </div>
-            <div>
-                <image-detail-list></image-detail-list>
-            </div>
-        </div>
-    `,
-    styles: [`
-
-.root-layout {
-    position: fixed;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    display: flex;
-    flex-direction: column;
-    flex-wrap: nowrap;
-    justify-content: flex-start;
-}
-
-.root-layout > div:nth-of-type(1) {
-    flex: 0 0 auto;
-}
-
-.root-layout > div:nth-of-type(2) {
-    flex: 1 1 auto;
-    position: relative;
-    overflow: auto;
-}
-
-`
-    ]
+    templateUrl: 'app/components/demo-app/layouts/detail-only-layout.html',
+    styleUrls: ['app/components/demo-app/layouts/detail-only-layout.css']
 })
 export class DetailOnlyLayout {
     @Input() public isLoading: boolean = true
