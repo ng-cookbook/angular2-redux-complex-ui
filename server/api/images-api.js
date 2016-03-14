3e10c9f46452317b6c42dba845001d751ad4d119
Dave Baskin <dbaskin@wintellect.com>
2016-03-14 19:29:13 -0400

    Updating images web API.

--- server/api/images-api.js
+++ server/api/images-api.js
@@ -1,13 +1,39 @@
 
 import express from 'express'
 import path from 'path'
+import {isMatchingText} from '../utils/string-utils'
+import imageData from '../data/images.json'
 
 const dataPath = path.join(__dirname, '../data')
 
 let router = express.Router()
 
 router.get('/images', (req, res) => {
-  res.sendFile(path.join(dataPath, 'images.json'))
+  res.json(imageData)
 })
+
+router.get('/images/:id', findImage, (req, res) => {
+  res.json(req.imageInfo)
+})
+
+router.get('/images/:id/image', findImage, (req, res) => {
+  let imageFile = path.join(dataPath, 'images', 'full', req.imageInfo.name)
+  res.sendFile(imageFile)
+})
+
+router.get('/images/:id/thumb', findImage, (req, res) => {
+  let imageFile = path.join(dataPath, 'images', 'thumbs', req.imageInfo.name)
+  res.sendFile(imageFile)
+})
+
+function findImage(req, res, next) {
+  let id = req.params.id
+  let imageInfo = imageData.find(img => isMatchingText(img.id, id))
+  if(!imageInfo) {
+    return res.sendStatus(404)
+  }
+  req.imageInfo = imageInfo
+  next()
+}
 
 export default router
---
+++ server/utils/string-utils.js
@@ -0,0 +1,7 @@
+
+export function isMatchingText(text1, text2) {
+  if(text1 == null || text2 == null) {
+    return false
+  }
+  return String(text1).trim().toLocaleLowerCase() === String(text2).trim().toLocaleLowerCase()
+}
