c001a02047fbc72582d4cb33113ff7f726e7bdaf
Dave Baskin <dbaskin@wintellect.com>
2016-03-15 20:01:13 -0400

    Updating isLoading flag on title bar.

--- src/app/components/demo-app/title-bar.ts
+++ src/app/components/demo-app/title-bar.ts
@@ -3,16 +3,26 @@ import {Component, Input} from 'angular2/core'
 import {RouterLink} from 'angular2/router'
 import {LoadingIndicator} from '../loading-indicator/loading-indicator'
 import {resetZurbFoundation} from '../../services/zurb-foundation-services'
+import {AppStoreSubscriber, IAppStoreSubscriber} from '../../decorators/app-store-subscriber'
 
 @Component({
     selector: 'title-bar',
     directives: [LoadingIndicator, RouterLink],
     templateUrl: 'app/components/demo-app/title-bar.html'
 })
-export class TitleBar {
+@AppStoreSubscriber()
+export class TitleBar implements IAppStoreSubscriber {
 
-    @Input() public isLoading: boolean = true
+    public isLoading: boolean = true
     public loadingMessage: string = 'Loading Images ...'
+
+    public onInitAppStoreSubscription(source: any): void {
+        return source
+            .map((state: any) => state.imageData.isLoading)
+            .subscribe((isLoading: boolean) => {
+                this.isLoading = isLoading
+            })
+    }
 
     public ngAfterViewInit() {
         resetZurbFoundation(); // To initialize menu functionality
