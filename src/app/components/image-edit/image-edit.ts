d52b11920d6cf1a016e1d329bb5d35de79441bae
Dave Baskin <dbaskin@wintellect.com>
2016-03-20 20:25:33 -0400

    Adding image title editor.

--- src/app/components/image-edit/image-edit.ts
+++ src/app/components/image-edit/image-edit.ts
@@ -1,6 +1,8 @@
 
 import {Component} from 'angular2/core'
+import {AppStore} from '../../services/app-store'
 import {AppStoreSubscriber, IAppStoreSubscriber} from '../../decorators/app-store-subscriber'
+import {changeImageTitle} from '../../actions/image-list-actions'
 
 @Component({
     selector: 'image-edit',
@@ -13,13 +15,21 @@ export class ImageEdit implements IAppStoreSubscriber {
 
     public image: any
 
+    constructor(private appStore: AppStore) {
+    }
+
     public onInitAppStoreSubscription(source: any): void {
         return source
             .filter((state: any) => state.imageData.currentImageId)
             .map((state: any) => ({
                 details: state.imageData.dataSet[state.imageData.currentImageId],
-                url: ['/api', 'images', state.imageData.currentImageId, 'image'].join('/')
+                url: ['/api', 'images', state.imageData.currentImageId, 'image'].join('/'),
+                title: state.imageData.dataSet[state.imageData.currentImageId].title
             }))
             .subscribe((image: any) => this.image = image)
+    }
+
+    public updateTitle(newTitle: string) {
+        this.appStore.dispatch(changeImageTitle(this.image.details.id, newTitle))
     }
 }
