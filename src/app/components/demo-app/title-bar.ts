9e06538d7ba3097002fb322a739455800807d093
Dave Baskin <dbaskin@wintellect.com>
2016-04-06 20:42:24 -0400

    Removing zurb foundation initialization.

--- src/app/components/demo-app/title-bar.ts
+++ src/app/components/demo-app/title-bar.ts
@@ -2,12 +2,11 @@
 import {Component} from 'angular2/core'
 import {RouterLink} from 'angular2/router'
 import {LoadingIndicator} from '../loading-indicator/loading-indicator'
-import {InitializeZurbFoundation} from '../../directives/init-zurb'
 import {AppStoreSubscriber, IAppStoreSubscriber} from '../../decorators/app-store-subscriber'
 
 @Component({
     selector: 'title-bar',
-    directives: [LoadingIndicator, InitializeZurbFoundation, RouterLink],
+    directives: [LoadingIndicator, RouterLink],
     templateUrl: 'app/components/demo-app/title-bar.html'
 })
 @AppStoreSubscriber()
