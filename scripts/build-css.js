4149952d7c11460da4d0ecb4f25955476428c905
Dave Baskin <dbaskin@wintellect.com>
2016-04-06 20:34:50 -0400

    Moving component CSS files to SASS and changing build.

--- scripts/build-css.js
+++ scripts/build-css.js
@@ -1,28 +1,39 @@
 import _ from 'lodash'
 import FS from 'fs'
 import Q from 'q'
+import glob from 'glob'
 import sass from 'node-sass'
 
+let globSearch = Q.denodeify(glob)
 let sassRender = Q.nbind(sass.render, sass)
-let writeFile = Q.nbind(FS.writeFile, FS)
+let writeFile = Q.denodeify(FS.writeFile)
 let sassDefaults = {
   includePaths: [
   ],
   indentedSyntax: true
 }
 
-buildSassFile('demo-app')
+globSearch('src/**/*.scss')
+  .then((files) => {
+    var result = Q()
+    files.forEach((srcFile) => {
+      result = result
+        .then(() => {
+          let dstFile = srcFile.replace(/\.scss$/i, '.css')
+          return buildSassFile(srcFile, dstFile)
+        })
+    })
+    return result
+  })
   .then(() => {
-    console.log('Compiled SASS CSS.')
+    console.log('Compiled CSS files.')
   })
   .catch((err) => {
     console.log('SASS Error:')
     console.log(err)
   })
 
-function buildSassFile (name) {
-  let srcFile = [ 'scss/', name, '.scss' ].join('')
-  let dstFile = [ 'src/css/', name, '.css' ].join('')
+function buildSassFile (srcFile, dstFile) {
   let sassOptions = _.defaults({ file: srcFile }, sassDefaults)
   return sassRender(sassOptions)
     .then((result) => {
