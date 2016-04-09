038c57447b595d474c4ff39e0a5c5ab8a6f8635a
Dave Baskin <dbaskin@wintellect.com>
2016-04-09 13:01:24 -0400

    Adding 'destroy' to semantic-ui menu.

--- src/app/directives/semanti-ui-init.ts
+++ src/app/directives/semanti-ui-init.ts
@@ -1,13 +1,17 @@
 
-import {Directive, ElementRef} from 'angular2/core'
+import {Directive, ElementRef, OnDestroy} from 'angular2/core'
 
 declare var $: any
 
 @Directive({
     selector: '.ui.dropdown'
 })
-export class InitializeDropdown {
-    constructor(el: ElementRef) {
-        $(el.nativeElement).dropdown();
+export class InitializeDropdown implements OnDestroy {
+    constructor(private el: ElementRef) {
+        $(this.el.nativeElement).dropdown();
+    }
+
+    ngOnDestroy() {
+        $(this.el.nativeElement).dropdown('destroy');
     }
 }
