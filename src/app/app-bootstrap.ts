9cfb13bfddb4e74a6b40b4dcb2a3b5530dfc96e5
Dave Baskin <dbaskin@wintellect.com>
2016-06-23 21:57:14 -0400

    Updates for new forms module.

--- src/app/app-bootstrap.ts
+++ src/app/app-bootstrap.ts
@@ -2,6 +2,7 @@
 import {bootstrap} from '@angular/platform-browser-dynamic'
 import {HTTP_PROVIDERS} from '@angular/http'
 import {ROUTER_PROVIDERS} from '@angular/router-deprecated'
+import {disableDeprecatedForms, provideForms} from '@angular/forms'
 import {DemoApp} from './components/demo-app/demo-app'
 
 import {provideAppStore, provideReducer} from './services/app-store'
@@ -11,5 +12,7 @@ bootstrap(DemoApp, [
     HTTP_PROVIDERS,
     ROUTER_PROVIDERS,
     provideReducer('imageData', imageData),
-    provideAppStore()
+    provideAppStore(),
+    disableDeprecatedForms(),
+    provideForms()
 ])
