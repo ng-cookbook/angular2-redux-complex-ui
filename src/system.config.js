ef750fb7b1faf3a45e24fcf7811955b089750a0a
Dave Baskin <dbaskin@wintellect.com>
2016-06-23 21:42:47 -0400

    Style updates.

--- src/system.config.js
+++ src/system.config.js
@@ -1,50 +1,49 @@
-(function(global) {
+(function (global) {
+  var map = {
+    '@angular': 'vendor/angular',
+    'rxjs': 'vendor/rxjs',
+    'lodash': 'vendor/lodash/lodash.js',
+    'redux': 'vendor/redux/redux.js',
+    'redux-thunk': 'vendor/redux-thunk/index.js'
+  }
 
-    var map = {
-        '@angular': 'vendor/angular',
-        'rxjs': 'vendor/rxjs',
-        'lodash': 'vendor/lodash/lodash.js',
-        'redux': 'vendor/redux/redux.js',
-        'redux-thunk': 'vendor/redux-thunk/index.js'
-    };
-
-    var packages = {
-        'app': {
-            main: 'app-bootstrap.js',
-            defaultExtension: 'js'
-        },
-        'rxjs': {
-            defaultExtension: 'js'
-        }
-    };
-
-    var ngPackageNames = [
-        'common',
-        'compiler',
-        'core',
-        'http',
-        'platform-browser',
-        'platform-browser-dynamic',
-        'router',
-        'router-deprecated'
-    ];
-
-    function packIndex(pkgName) {
-        packages['@angular/'+pkgName] = { main: 'index.js', defaultExtension: 'js' };
+  var packages = {
+    'app': {
+      main: 'app-bootstrap.js',
+      defaultExtension: 'js'
+    },
+    'rxjs': {
+      defaultExtension: 'js'
     }
-    function packUmd(pkgName) {
-        packages['@angular/'+pkgName] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' };
-    }
+  }
 
-    var setPackageConfig = System.packageWithIndex ? packIndex : packUmd;
-    ngPackageNames.forEach(setPackageConfig);
-    var config = {
-        map: map,
-        packages: packages
-    };
-    System.config(config);
-    System
-        .import('app/app-bootstrap')
-        .then(null, console.error.bind(console));
+  var ngPackageNames = [
+    'common',
+    'compiler',
+    'core',
+    'http',
+    'platform-browser',
+    'platform-browser-dynamic',
+    'router',
+    'router-deprecated'
+  ]
 
-}(this));
+  function packIndex (pkgName) {
+    packages[ '@angular/' + pkgName ] = { main: 'index.js', defaultExtension: 'js' }
+  }
+
+  function packUmd (pkgName) {
+    packages[ '@angular/' + pkgName ] = { main: '/bundles/' + pkgName + '.umd.js', defaultExtension: 'js' }
+  }
+
+  var setPackageConfig = System.packageWithIndex ? packIndex : packUmd
+  ngPackageNames.forEach(setPackageConfig)
+  var config = {
+    map: map,
+    packages: packages
+  }
+  System.config(config)
+  System
+    .import('app/app-bootstrap')
+    .then(null, console.error.bind(console))
+}(this))
