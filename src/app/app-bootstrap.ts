4ab0f3d72e22329003d68802a3b2b56b4018ba54
Dave Baskin <dfbaskin@gmail.com>
2016-08-20 12:39:21 -0400

    Updating packages and bootstrap for Angular RC5.

--- src/app/app-bootstrap.ts
+++ src/app/app-bootstrap.ts
@@ -1,18 +1,35 @@
 
-import {bootstrap} from '@angular/platform-browser-dynamic'
-import {HTTP_PROVIDERS} from '@angular/http'
+import {NgModule} from '@angular/core'
+import {platformBrowserDynamic} from '@angular/platform-browser-dynamic'
+import {BrowserModule} from '@angular/platform-browser'
+import {HttpModule} from '@angular/http'
 import {APP_ROUTER_PROVIDERS} from './app-routes'
-import {disableDeprecatedForms, provideForms} from '@angular/forms'
+import {FormsModule} from '@angular/forms'
 import {DemoApp} from './demo-app'
 
 import {provideAppStore, provideReducer} from './services/app-store'
 import {imageData} from './reducers/image-list'
 
-bootstrap(DemoApp, [
-    HTTP_PROVIDERS,
-    APP_ROUTER_PROVIDERS,
-    provideReducer('imageData', imageData),
-    provideAppStore(),
-    disableDeprecatedForms(),
-    provideForms()
-])
+@NgModule({
+    declarations: [
+        DemoApp
+    ],
+    imports: [
+        BrowserModule,
+        HttpModule,
+        FormsModule
+    ],
+    providers: [
+        APP_ROUTER_PROVIDERS,
+        provideReducer('imageData', imageData),
+        provideAppStore()
+    ],
+    bootstrap: [
+        DemoApp
+    ]
+})
+export class AppModule { }
+
+platformBrowserDynamic().bootstrapModule(AppModule);
+
+
