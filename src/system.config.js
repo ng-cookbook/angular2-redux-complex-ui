ffe18ebf17509bb681ffa737caa821ecfa2faa26
Dave Baskin <dfbaskin@gmail.com>
2016-09-07 21:06:25 -0400

    Fixing references to node_modules files.

--- src/system.config.js
+++ src/system.config.js
@@ -2,22 +2,22 @@
 
     System.config({
         paths: {
-            'npm:': 'node_modules/'
+            'npm:': 'vendor/'
         },
         map: {
             'app': 'app',
-            '@angular/core': 'npm:@angular/core/bundles/core.umd.js',
-            '@angular/common': 'npm:@angular/common/bundles/common.umd.js',
-            '@angular/compiler': 'npm:@angular/compiler/bundles/compiler.umd.js',
-            '@angular/platform-browser': 'npm:@angular/platform-browser/bundles/platform-browser.umd.js',
-            '@angular/platform-browser-dynamic': 'npm:@angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
-            '@angular/http': 'npm:@angular/http/bundles/http.umd.js',
-            '@angular/router': 'npm:@angular/router/bundles/router.umd.js',
-            '@angular/forms': 'npm:@angular/forms/bundles/forms.umd.js',
-            'rxjs': 'vendor/rxjs',
-            'lodash': 'vendor/lodash/lodash.js',
-            'redux': 'vendor/redux/redux.js',
-            'redux-thunk': 'vendor/redux-thunk/index.js'
+            '@angular/core': 'npm:angular/core/bundles/core.umd.js',
+            '@angular/common': 'npm:angular/common/bundles/common.umd.js',
+            '@angular/compiler': 'npm:angular/compiler/bundles/compiler.umd.js',
+            '@angular/platform-browser': 'npm:angular/platform-browser/bundles/platform-browser.umd.js',
+            '@angular/platform-browser-dynamic': 'npm:angular/platform-browser-dynamic/bundles/platform-browser-dynamic.umd.js',
+            '@angular/http': 'npm:angular/http/bundles/http.umd.js',
+            '@angular/router': 'npm:angular/router/bundles/router.umd.js',
+            '@angular/forms': 'npm:angular/forms/bundles/forms.umd.js',
+            'rxjs': 'npm:rxjs',
+            'lodash': 'npm:lodash/lodash.js',
+            'redux': 'npm:redux/redux.js',
+            'redux-thunk': 'npm:redux-thunk/index.js'
         },
         packages: {
             app: {
